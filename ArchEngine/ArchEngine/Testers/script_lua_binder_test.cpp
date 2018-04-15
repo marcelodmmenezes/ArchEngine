0ea626127c463d523c925c3e51608304a834052c
marcelodmmenezes <marcelodmmenezes@gmail.com>
2018-04-15 13:53:22 -0300

    Updated Makefile for luaBinder

---
+++ ArchEngine/ArchEngine/Testers/script_lua_binder_test.cpp
@@ -0,0 +1,20 @@
+/*===========================================================================*
+ * Arch Engine - "Testers/script_lua_binder_test.cpp"                        *
+ *                                                                           *
+ * This is a tester file. It's purpose is to test some functionalities, thus *
+ * should be removed from releases.                                          *
+ * Expect some hard coded, bizarre stuff. You've been warned ;)              *
+ *                                                                           *
+ * Marcelo de Matos Menezes - marcelodmmenezes@gmail.com                     *
+ * Created: 15/04/2018                                                       *
+ * Last Modified: 15/04/2018                                                 *
+ *===========================================================================*/
+
+
+#include "../Utils/serviceLocator.hpp"
+#include "../Script/luaBinder.hpp"
+
+
+int main() {
+	
+}
